5a6c9cc6dca1df4fcb6eef2571b376ecbce17d26
belgrades <fcremaldc@gmail.com>
2016-08-08 15:57:31 -0400

    Adding __init__.py

---
+++ sabermetrics/__init__.py
@@ -0,0 +1,13 @@
+"""Sabermetrics: A python package with a set of baseball analytic functions for sabermetric purposes.
+
+.. moduleauthor:: Fernando Crema <fcremaldc@gmail.com>
+
+"""
+
+import sabermetrics.pitching
+import sabermetrics.fielding
+import sabermetrics.batting
+
+
+def start():
+    "Initializing modules."
