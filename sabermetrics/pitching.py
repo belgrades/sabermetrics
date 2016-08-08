e463af78d7e134ab1660797cf183328dbf420e2f
belgrades <fcremaldc@gmail.com>
2016-08-08 18:34:22 -0400

    Adding new comments with MathJax to pitching.py

--- sabermetrics/pitching.py
+++ sabermetrics/pitching.py
@@ -8,8 +8,12 @@
 
 
 def era(er, outs):
-    """Represents the number of earned runs a pitcher allows per nine innings -- with earned runs being any runs that
+    r"""Represents the number of earned runs a pitcher allows per nine innings -- with earned runs being any runs that
     scored without the aid of an error or a passed ball.
+
+    .. math::
+        BABIP = \frac{27 ER}{Outs}
+
     :param er: Earned runs.
     :type er: int.
     :param outs: Number of batters and baserunners that are put out while the pitcher is on the mound.
