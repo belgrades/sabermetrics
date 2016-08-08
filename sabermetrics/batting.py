e517d9291a2f1b6677f5814fb299f70c018bebfc
belgrades <fcremaldc@gmail.com>
2016-08-08 18:32:54 -0400

    Adding new comments with MathJax and BABIP

--- sabermetrics/batting.py
+++ sabermetrics/batting.py
@@ -8,8 +8,12 @@
 
 
 def avg(h, ab):
-    """batting average is determined by dividing a player's hits by his total at-bats for a number between zero
+    r"""Batting average is determined by dividing a player's hits by his total at-bats for a number between zero
     (shown as .000) and one (1.000).
+
+    .. math::
+        AVG = \frac{H}{AB}
+
     :param h: Hits.
     :type h: int.
     :param ab: At bats.
@@ -22,5 +26,34 @@ def avg(h, ab):
         return h*1.0/ab
     except ZeroDivisionError:
         print("At bats must be greater than 0.")
+        import sys
+        sys.exit(-1)
+
+
+def babip(h, hr, ab, k, sf):
+    r"""BABIP measures a player's batting average exclusively on balls hit into the field of play, removing outcomes
+    not affected by the opposing defense (namely home runs and strikeouts).
+
+    .. math::
+        BABIP = \frac{H-HR}{AB-K-HR-SF}
+
+    :param h: Hits.
+    :type h: int.
+    :param hr: Homeruns.
+    :type hr: int.
+    :param ab: At bats.
+    :type ab: int.
+    :param k: Strikeouts.
+    :type k: int.
+    :param sf: Sacrifice fly.
+    :type sf: int.
+    :returns:  float -- Batting average balls in play..
+    :raises: ZeroDivisionError
+    """
+
+    try:
+        return (h-hr)*1.0/(ab-k-hr-sf)
+    except ZeroDivisionError:
+        print('Balls in play must be greater than 0')
         import sys
         sys.exit(-1)
